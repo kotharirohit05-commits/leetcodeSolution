567a82a79ab46a9a22681bebf94b82a1f243f70c
kotharirohit05-commits <kothari.rohit05@gmail.com>
2025-08-23 00:02:16 +0530

    Time: 6 ms (30.65%), Space: 46.2 MB (46.6%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for (int num : nums) {
-            ans ^= num;  
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i]; 
+            }
         }
-        return ans;
+        return nums[nums.length - 1]; 
     }
 }
