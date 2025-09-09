cba19864f80b830574216238b1a02eff82b7f840
kotharirohit05-commits <kothari.rohit05@gmail.com>
2025-09-09 15:15:09 +0530

    Time: 2 ms (96.69%), Space: 42 MB (38.5%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character>stack = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(c == '('){
-                stack.push(')');
-            }
-            else if(c == '{'){
+        Stack<Character> stack = new Stack<>();
+        int l = s.length();
+        if(l%2 !=0) return false;
+
+        for(char c :s.toCharArray()){
+            if(c == '{')
                 stack.push('}');
-            }
-            else if(c == '['){
+            else if (c == '(')
+                stack.push(')');
+            else if (c =='[')
                 stack.push(']');
-            }
+            
             else{
                 if(stack.isEmpty() || stack.pop() != c){
-                    return false;
-                }
+                return false;
+            }
             }
         }
         return stack.isEmpty();
-        
+
     }
 }
