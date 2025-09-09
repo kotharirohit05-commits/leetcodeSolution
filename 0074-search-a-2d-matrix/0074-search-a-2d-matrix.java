e9549c3955aa83ee842ae790c96a1b492795fc0a
kotharirohit05-commits <kothari.rohit05@gmail.com>
2025-09-09 14:05:00 +0530

    Time: 0 ms (100%), Space: 42.9 MB (5.4%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = matrix.length;
+        int c= matrix[0].length ;
+        int i = 0 ;
+        int j = c-1;
+        while(i < r && j >= 0){
+            if(matrix[i][j] == target) return true;
+            else if(matrix[i][j] > target) j--;
+            else if(matrix[i][j] < target) i++;
+            
+
+        }
+        return false;
+
+    }
+}
