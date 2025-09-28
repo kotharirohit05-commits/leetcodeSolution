22ba1c641d6bd1b83496d8edcc377ca12be7fa5a
kotharirohit05-commits <kothari.rohit05@gmail.com>
2025-09-28 16:38:09 +0530

    Time: 22 ms (99.35%), Space: 43.7 MB (25.29%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -1,22 +1,18 @@
-import java.util.*;
-
 class Solution {
     public int triangleNumber(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
         int count = 0;
-
-        for (int k = n - 1; k >= 2; k--) {
-            int i = 0, j = k - 1;
-            while (i < j) {
-                if (nums[i] + nums[j] > nums[k]) {
-                    count += j - i;
-                    j--;
-                } else {
-                    i++;
-                }
+        for(int k = n-1 ; k>= 2 ; k--){
+            int i = 0 , j = k-1;
+        while(i < j)
+            if(nums[i] + nums[j] > nums[k]){
+                count += j - i;
+                j--;
+            }else{
+                i++;
             }
         }
-        return count;
+            return count ;
     }
 }
