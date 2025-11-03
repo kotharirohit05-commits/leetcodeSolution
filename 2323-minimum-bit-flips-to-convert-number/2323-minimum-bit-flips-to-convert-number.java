72b682e0a91fc0d51674e5e6b94c8b35d1bb2f68
kotharirohit05-commits <kothari.rohit05@gmail.com>
2025-11-03 13:41:38 +0530

    Time: 0 ms (100%), Space: 42.3 MB (6.58%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int ans = start ^ goal;
+        int count = 0;
+        while(ans !=0){
+            if(ans % 2 == 1){
+                count += 1;
+            }
+            ans /= 2;
+        }
+        return count;
+
+    }
+}
