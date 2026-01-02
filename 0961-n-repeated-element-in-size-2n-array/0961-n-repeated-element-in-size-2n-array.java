deb924c9ac2921cf05da1c5fa5db909ec204639b
kotharirohit05-commits <kothari.rohit05@gmail.com>
2026-01-03 01:19:23 +0530

    Time: 0 ms (100%), Space: 47.9 MB (6.18%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -3,18 +3,10 @@ class Solution {
         HashMap<Integer,Integer> map1 = new HashMap<>();
         for(int n : nums){
             map1.put(n , map1.getOrDefault(n , 0 ) + 1);
-        }
-        ArrayList<Integer> res = new ArrayList<>();
-        for(int s : map1.keySet()){
-            if(map1.get(s) > 1){
-                res.add(s);
+            if(map1.get(n) > 1){
+                return n;
             }
         }
-        int[] ans = new int[1];
-        for(int i = 0 ; i <= 0 ; i++){
-            ans[i] = res.get(i);
-        }
-        int a = ans[0];
-        return a;
+        return -1;
     }
 }
