d60e00dbf7583f0b7e091507944ea0e6a920f0e8
kotharirohit05-commits <kothari.rohit05@gmail.com>
2026-01-10 00:45:03 +0530

    Time: 1 ms (74.82%), Space: 42.4 MB (19.84%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isThree(int n) {
+        int count = 1;
+        for(int i = 2 ; i <= n ; i++){
+            if(n % i == 0){
+                count += 1;
+            }
+        }
+        return(count == 3);
+    }
+}
