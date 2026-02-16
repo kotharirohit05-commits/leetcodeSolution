8fb9b5903efe78a5972239c1ada9c71673d5f399
kotharirohit05-commits <kothari.rohit05@gmail.com>
2026-02-16 16:47:40 +0530

    Time: 3 ms (100%), Space: 46.1 MB (36.48%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,7 +1,7 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
         int count = 0;
-        while(left < right ){
+        while(left != right ){
             left = left >> 1;
             right = right >> 1;
             count++;
