b973019c0278695f3d6e86ee80bd64f2c5642512
kotharirohit05-commits <kothari.rohit05@gmail.com>
2026-03-09 22:17:41 +0530

    Time: 24 ms (54.37%), Space: 76.3 MB (83.27%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        HashSet<Integer> set = new HashSet<>();
+        int l  = 0;
+        for(int i = 0 ; i < nums.length  ; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+            if(set.size() > k){
+                set.remove(nums[l]);
+                l++;
+            }
+        }
+        return false;
+
+    }
+}
