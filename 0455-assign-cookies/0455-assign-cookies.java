f3964d2ed741552703b9670f3755966bc563c495
kotharirohit05-commits <kothari.rohit05@gmail.com>
2026-04-03 23:34:30 +0530

    Time: 16 ms (12.32%), Space: 50.8 MB (98.62%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        boolean arr[] = new boolean[g.length];
+
+        Arrays.fill(arr,true);
+        int count = 0;
+        int j = 0;
+        for(int i = 0 ; i < s.length ; i++){
+            if(s[i] >= g[j]){
+                count++;
+                j++;
+                if(j == g.length) break;
+            }
+        }
+        return count;
+    }
+}
