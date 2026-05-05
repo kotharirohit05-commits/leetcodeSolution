d661eb06caba61c07cafb4a2fa8a3dfed7d227e8
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-05-05 19:32:53 +0530

    Time: 0 ms (100%), Space: 44.5 MB (25.1%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k == 0) return head;
+        int len = 1;
+        ListNode tail = head;
+        while(tail.next != null){
+            tail = tail.next;
+            len++;
+        }
+        k = k % len ;
+        if(k == 0) return head;
+        int places = len - k -1 ;
+
+        ListNode temp = head;
+        
+        for(int i = 0 ; i < places ; i++){
+            temp = temp.next;
+        }
+        ListNode newhead = temp.next;
+
+        temp.next = null;
+        tail.next = head;  
+        return newhead;
+
+    }
+}
