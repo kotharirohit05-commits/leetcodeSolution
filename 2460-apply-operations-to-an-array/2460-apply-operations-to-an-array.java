a156239b65e7fbf806666a5f37fda0b3ab32871d
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-06-03 22:07:55 +0530

    Time: 1 ms (95.17%), Space: 45.2 MB (61.35%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -6,13 +6,15 @@ class Solution {
                 nums[i + 1] = 0;
             }
         }
-        int[] ans = new int[nums.length];
         int k =0 ;
         for(int i = 0 ; i < nums.length ; i++){
             if(nums[i] != 0){
-                ans[k++] = nums[i];
+                nums[k++] = nums[i];
             }
         }
-        return ans;
+        while(k < nums.length){
+            nums[k++] = 0;
+        }
+        return nums;
     }
 }
