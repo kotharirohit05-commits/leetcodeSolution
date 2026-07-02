d779cb1f90e817a7562c9173b60fcd74222df8ef
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-07-02 16:17:25 +0530

    Time: 61 ms (70.36%), Space: 107.8 MB (14.31%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,7 +1,9 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        
         Stack<Integer> stack = new Stack<>();
         int[] ans = new int[temperatures.length];
+
         for(int i = 0 ; i < temperatures.length ; i++){
         while( !stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
             int idx = stack.pop();
