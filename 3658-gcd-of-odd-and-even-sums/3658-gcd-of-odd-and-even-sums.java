bc664fff287bd301f90f09047ca030966a272bc3
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-07-15 13:57:11 +0530

    Time: 0 ms (100%), Space: 42.4 MB (74.78%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -2,16 +2,7 @@ class Solution {
     public int gcdOfOddEvenSums(int n) {
        
         
-        int so = n*n;
-        int se = n*(n+1);
-        int k = Math.max(so,se);
-        int m = Math.min(so,se);
-        for(int i = m; i>=1 ; i-- ){
-            if(k%i==0 && m%i==0){
-                return i;
-            }
-        }
-        return 1;
+        return n;
             
     }
 }
