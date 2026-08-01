146029c87c170f01e896375b36642b2e5728c356
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-08-01 18:59:21 +0530

    Time: 0 ms (100%), Space: 42.9 MB (41.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        Integer[][] dp = new Integer[n][n];
+        return solve(nums, 0, n - 1, dp) >= 0;
+    }
+
+    private int solve(int[] nums, int i, int j, Integer[][] dp) {
+        if (i == j) {
+            return nums[i];
+        }
+
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+
+        int takeLeft = nums[i] - solve(nums, i + 1, j, dp);
+        int takeRight = nums[j] - solve(nums, i, j - 1, dp);
+
+        dp[i][j] = Math.max(takeLeft, takeRight);
+        return dp[i][j];
+    }
+}
