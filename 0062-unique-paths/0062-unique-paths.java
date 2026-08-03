27562da00903690eba8148395fca6c4a813427a7
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-08-03 11:01:29 +0530

    Time: 1 ms (14.4%), Space: 42 MB (83.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,28 +1,22 @@
 class Solution {
+    public int uniquePaths(int r, int c) {
+        int[][] dp = new int[r][c];
 
-    private int helper(int r, int c, int[][] dp){
-        
-        for(int i = 0; i <= r; i++){
-            for(int j = 0; j <= c; j++){
-                if(i == 0 && j == 0) dp[i][j] = 1;
-                else{
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (i == 0 && j == 0)
+                    dp[i][j] = 1;
+                else {
                     int up = 0;
                     int left = 0;
-                    if(i > 0)  up   = dp[i-1][j];
-                    if(j > 0)  left = dp[i][j-1];
+                    if (i > 0)
+                        up = dp[i - 1][j];
+                    if (j > 0)
+                        left = dp[i][j - 1];
                     dp[i][j] = up + left;
                 }
             }
         }
-
-        return dp[r][c];
-
-
-    }
-
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        
-        return helper(m - 1, n - 1 , dp);
+        return dp[r-1][c-1];
     }
 }
