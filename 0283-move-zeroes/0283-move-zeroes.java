eefec0a80899cc44a4aad99fee5b38c3f18aea49
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-08-05 18:24:12 +0530

    Time: 2 ms (92.13%), Space: 47.9 MB (20.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        
         int n = nums.length;
         int j = 0;
-         for(int i = 0 ; i < n ; i++){
+        for(int i = 0; i < n; i++){
             if(nums[i] != 0){
-                int temp = nums[i];
-                nums[i]  = nums[j];
-                nums[j] = temp;
-                j+=1;
-                
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
             }
-            
-         }
-         
-        
+        }
+
 
     }
 }
