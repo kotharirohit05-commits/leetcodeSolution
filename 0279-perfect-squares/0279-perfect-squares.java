f00086fd1b61fb7de7af05983e715da886798b09
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-08-07 23:28:32 +0530

    Time: 62 ms (34.01%), Space: 43.8 MB (94.89%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    private int helper(int n, int[] dp){
+        if(n == 0) return 0;
+
+        if(dp[n] != -1) return dp[n];
+
+        int ans = Integer.MAX_VALUE;
+        for(int i = 1; i * i <= n; i++){
+            int res = 1 + helper(n - i*i, dp);
+            ans = Math.min(ans, res);
+        }
+        
+        return dp[n] = ans;
+
+    }
+
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
+    }
+}
