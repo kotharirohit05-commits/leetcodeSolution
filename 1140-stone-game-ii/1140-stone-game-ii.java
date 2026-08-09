2e1faceef2bde39e478d52160a361d867cb0cf63
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-08-09 23:47:40 +0530

    Time: 4 ms (73.16%), Space: 44.5 MB (59.83%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    int[][] dp;
+    int[] suffix;
+    int n;
+
+    public int stoneGameII(int[] piles) {
+
+        n = piles.length;
+
+        dp = new int[n][n + 1];
+        suffix = new int[n + 1];
+
+        for(int i = n - 1; i >= 0; i--){
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        return helper(0, 1);
+    }
+
+    private int helper(int i, int M) {
+
+        if(i >= n){
+            return 0;
+        }
+
+        if(dp[i][M] != 0){
+            return dp[i][M];
+        }
+
+        int ans = 0;
+
+        for(int X = 1; X <= 2 * M && i + X <= n; X++){
+
+            int newM = Math.max(M, X);
+
+            int current = suffix[i]
+                         - helper(i + X, newM);
+
+            ans = Math.max(ans, current);
+        }
+
+        dp[i][M] = ans;
+
+        return ans;
+    }
+}
