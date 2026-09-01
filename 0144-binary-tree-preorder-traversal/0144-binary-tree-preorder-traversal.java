54c0c7c2df24d199b8d2cda00c5c9d492a227a35
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-09-01 11:10:47 +0530

    Time: 0 ms (100%), Space: 43 MB (85.44%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -15,20 +15,17 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        if(root == null) return ans;
-        stack.push(root);
-        while(!stack.isEmpty()){
-            root = stack.pop();
-            ans.add(root.val);
-            if(root.right != null){
-                stack.push(root.right);
-            }
-            if(root.left != null){
-                stack.push(root.left);
-            }
-        }
-        return ans;
+
+        List<Integer> list = new ArrayList<>();        
+        if(root == null) return list;
+        helper(root, list);
+        return list;
+
+    }
+    private void helper(TreeNode root, List<Integer> list){
+        if(root == null) return;
+        list.add(root.val);
+        helper(root.left, list);
+        helper(root.right, list);
     }
 }
