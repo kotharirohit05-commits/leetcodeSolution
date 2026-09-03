48722beab2bbe0ef21bc3aca25b25c030376b27a
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-09-03 13:54:42 +0530

    Time: 16 ms (91.98%), Space: 82.7 MB (47.97%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -15,28 +15,28 @@ class Solution {
         int ans = -1;
 
         while (low <= high) {
+
             int mid = low + (high - low) / 2;
-
             int bouquets = 0, flowers = 0;
-
-            for (int d : bloomDay) {
-                if (d <= mid) {
+            for(int d: bloomDay){
+                if(d <= mid){
                     flowers++;
-                    if (flowers == k) {
+                    if(flowers == k){
                         bouquets++;
                         flowers = 0;
                     }
-                } else {
-                    flowers = 0; 
+                }else{
+                    flowers = 0;
                 }
             }
 
-            if (bouquets >= m) {
+            if(bouquets >= m){
                 ans = mid;
                 high = mid - 1;
-            } else {
-                low = mid + 1;  
+            }else{
+                low = mid + 1;
             }
+
         }
 
         return ans;
