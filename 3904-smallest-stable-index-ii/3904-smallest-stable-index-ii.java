1faca879a6ffca6e0e6eddf2e65deb33855d22ab
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-09-05 07:19:43 +0530

    Time: 3 ms (100%), Space: 133.4 MB (14.89%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+
+        int[] suff = new int[n];
+        suff[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suff[i] = Math.min(nums[i], suff[i + 1]);
+        }
+
+        int pref = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            pref = Math.max(pref, nums[i]);  
+
+            if ((pref - suff[i]) <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
