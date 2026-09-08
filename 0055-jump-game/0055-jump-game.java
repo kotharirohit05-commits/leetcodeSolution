59c0e7f0f45827aba964a00e7bd95c2c37c32c4e
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-09-08 21:54:48 +0530

    Time: 2 ms (89.09%), Space: 47.6 MB (95.4%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
-        int idx = 0;
-
+        int n = nums.length;
+        int max = 0;
         for(int i = 0; i < nums.length; i++){
-            if(i > idx) return false;
-            idx = Math.max(idx, i + nums[i]);
-            if(idx >= nums.length-1) return true;
+            if( i > max) return false;
+            max = Math.max(max, i + nums[i]);
         }
-
-        return false;
-
+        return true;
     }
 }
