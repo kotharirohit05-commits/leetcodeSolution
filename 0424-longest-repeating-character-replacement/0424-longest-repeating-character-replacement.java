33026caf406ee954329f041e73efd9a9a4c2abb6
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-09-08 17:58:44 +0530

    Time: 7 ms (88.92%), Space: 46.1 MB (78.84%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,20 +1,20 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int[] freq = new int[26];
-        int mF = 0;
-        int mL = 0;
+        int mf = 0;
         int l = 0;
-
-        for(int r = 0; r < s.length() ; r++){
+        int ans = 0;
+        for(int r = 0; r < s.length(); r++){
             char ch = s.charAt(r);
             freq[ch - 'A']++;
-            mF = Math.max(mF, freq[ch-'A']);
-            while( ( r + 1 - l ) - mF > k ){
-                    freq[s.charAt(l) - 'A']--;
-                    l++;
+            mf = Math.max(mf, freq[ch - 'A']);
+
+            while((r - l + 1) - mf > k){
+                freq[s.charAt(l) - 'A']--;
+                l++;
             }
-            mL = Math.max(mL, r - l + 1);
+            ans = Math.max(ans, r - l + 1);
         }
-        return mL;
+        return ans;
     }
 }
