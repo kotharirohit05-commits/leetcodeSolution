8a2d367af82bab957cb20ea020c56d7d03e308ee
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-09-09 11:28:43 +0530

    Time: 6 ms (3.31%), Space: 44.2 MB (55.7%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,52 +3,52 @@ class Solution {
         int row;
         int col;
         int time;
-        Pair(int row,int col,int time){
+        Pair(int row, int col, int time){
             this.row = row;
             this.col = col;
             this.time = time;
         }
     }
     public int orangesRotting(int[][] grid) {
-        int normaloranges = 0;
         int n = grid.length;
         int m = grid[0].length;
-        int[][] visi = new int[n][m];
         Queue<Pair> queue = new LinkedList<>();
+        int[][] visi = new int[n][m];
+        int normalOranges = 0;
 
-        for(int i = 0; i < n ; i++){
-            for(int j = 0 ; j < m; j++){
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
                     visi[i][j] = 2;
-                    queue.add(new Pair(i, j, 0));
+                    queue.add(new Pair(i,j,0));
                 }else if(grid[i][j] == 1){
-                    normaloranges++;
-                }else{
-                    visi[i][j] = 0;
+                    normalOranges++;
                 }
             }
         }
-        int time = 0;
-        int count = 0;
 
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, 1, 0, -1};
+        int count = 0;
+        int time = 0;
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+        System.out.println(count);
+
         while(!queue.isEmpty()){
             int r = queue.peek().row;
             int c = queue.peek().col;
             int t = queue.peek().time;
             queue.remove();
-            time = Math.max(time , t);
-            for(int i = 0; i < 4 ; i++){
-                int nr = r + drow[i];
-                int nc = c + dcol[i];
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1 && visi[nr][nc] == 0){
-                    queue.add(new Pair(nr, nc, t + 1));
+            time = Math.max(time, t);
+            for(int i = 0; i < 4; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && visi[nr][nc] == 0 && grid[nr][nc] == 1){
                     visi[nr][nc] = 2;
+                    queue.add(new Pair(nr, nc, time + 1));
                     count++;
                 }
             }
-        }if(count != normaloranges) return -1;
-        return time;
+        }
+        return count == normalOranges ? time : -1;
     }
 }
