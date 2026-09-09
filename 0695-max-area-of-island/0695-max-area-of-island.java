224dfcb38e4954f38e712c74558c9d1b836fedec
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-09-09 17:30:53 +0530

    Time: 4 ms (10.12%), Space: 47 MB (7.38%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,46 +1,59 @@
 class Solution {
-    private int dfs(int row, int col, int[][] copy, int[][] grid){
-        
-        copy[row][col] = 1;
 
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, 1, 0, -1};
-
-        int n = grid.length;
-        int m = grid[0].length;
-        int area = 1;
-
-        for(int i = 0; i < 4; i++){
-            int nr = row + drow[i];
-            int nc = col + dcol[i];
-            if(nr >= 0 && nr < n && nc >= 0 && nc < m && copy[nr][nc] == 0 && grid[nr][nc] == 1){
-                
-                area += dfs(nr, nc ,copy, grid);
-            }
+    class Pair{
+        int first;
+        int second;
+        Pair(int first, int second){
+            this.first = first;
+            this.second = second;
         }
-        return area;
-
     }
 
+    private int bfs(int[][] grid, int[][] visi, int row, int col){
 
+        visi[row][col] = 1;
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(row, col));
+
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, -1, 0, 1};
+
+        int count = 0;
+
+        while(!queue.isEmpty()){
+            int r = queue.peek().first;
+            int c = queue.peek().second;
+            queue.remove();
+            count++;
+            for(int i = 0; i < 4; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1 && visi[nr][nc] == 0){
+                    visi[nr][nc] = 1;
+                    queue.add(new Pair(nr, nc));
+                }
+            }
+        }
+
+        return count;
+
+    }
 
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] copy = new int[n][m];
-        int maxArea =0;
-
+        int[][] visi = new int[n][m];
+        int ans = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1 && copy[i][j] == 0){
-                     maxArea = Math.max(
-                        maxArea,
-                        dfs(i, j, copy, grid)
-                    );
+                if(grid[i][j] == 1 && visi[i][j] == 0){
+                    ans = Math.max(ans, bfs(grid, visi, i, j));
                 }
             }
         }
-        return maxArea;
-        
+        return ans;
     }
 }
