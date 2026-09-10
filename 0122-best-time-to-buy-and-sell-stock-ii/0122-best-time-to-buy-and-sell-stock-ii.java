0dfc3321d2d112f7cc93bdeda606fe57e7cb0021
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-09-10 14:59:03 +0530

    Time: 7 ms (5.38%), Space: 55.1 MB (5.39%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,37 +1,30 @@
 class Solution {
 
-    private int helper(int idx , int buy, int[] prices, int[][] dp){
+    private int helper(int idx, int buy, int[] prices, int[][] dp){
 
-        int n = prices.length;
-        int profit = 0;
-        
-        if(idx == n) return 0;
+        if(idx == prices.length) return 0;
+
         if(dp[idx][buy] != -1) return dp[idx][buy];
 
+        int profit = 0;
+
         if(buy == 1){
-            profit = Math.max(-prices[idx] + helper(idx + 1, 0, prices,dp) , 
-                            0 + helper(idx + 1, 1, prices,dp));
+            profit = Math.max(-prices[idx] + helper( idx + 1, 0, prices, dp), helper(idx + 1, 1, prices, dp));
         }else{
-            profit = Math.max(prices[idx] + helper(idx + 1, 1, prices,dp) ,
-                                0 + helper(idx + 1, 0, prices,dp));
+            profit = Math.max(prices[idx] + helper(idx , 1, prices, dp), helper(idx + 1, 0, prices, dp));
         }
 
-        
 
         return dp[idx][buy] = profit;
 
     }
 
     public int maxProfit(int[] prices) {
-
         int n = prices.length;
         int[][] dp = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        for(int[] x : dp){
+            Arrays.fill(x, -1);
         }
-
         return helper(0, 1, prices, dp);
-
     }
 }
