0bb87d8c2ded4cdacede97749c0db31dd3a85316
Kotha Rohit Kothari <23311a6682@aiml.sreenidhi.edu.in>
2026-09-10 13:56:42 +0530

    Time: 1 ms (99.95%), Space: 94.5 MB (51.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = Integer.MAX_VALUE;
-        int ans = 0;
-        for(int i = 0; i < prices.length; i++ ){
-            int cost = prices[i];
-            if(cost < min){
-                min = cost;
+        int buy = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < buy){
+                buy = prices[i];
+            }else{
+                profit = Math.max(profit, prices[i] - buy);
             }
-            else{
-                ans = Math.max(ans, cost - min);
-            }
-            
             
         }
-        return ans;
+
+        return profit;
     }
 }
